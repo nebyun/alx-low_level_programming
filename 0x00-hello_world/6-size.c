eba8620237685f4cc6909abce9d29d6d3384f035
nebyun <nebayifru@gmail.com>
2023-02-16 08:27:58 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-	sizeof();
+/*Declare data types*/
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+/*for printing output*/
+	printf("Size of int: %i bytes\t", sizeof(intType));
+	printf("Size of float: %f bytes\t", sizeof(floatType));
+	printf("Size of double: %g bytes\t", sizeof(doubleType));
+	printf("Size of char: %c bytes\t", sizeof(charType));
 	return (0);
 }
