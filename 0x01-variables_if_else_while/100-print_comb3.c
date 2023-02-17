16a5ec84f1b21ad3a87f18c3bbcdb208588dba99
nebyun <nebayifru@gmail.com>
2023-02-17 11:22:18 -0800

    Inventing is a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,16 @@ int main(void)
 {
 	int int1, int2;
 
-	for (int1 = 0 ; int1 < 10 ; int1++)
+	for (int1 = 0 ; int1 < 9 ; int1++)
 	{
-		for (int2 = 0 ; int2 <= 10 ; int2++)
+		for (int2 = 0 ; int2 < 10 ; int2++)
 		{
 			putchar(int1 % 10 + '0');
 			putchar(int2 % 10 + '0');
-			
-			if (int1 == 9 && int2 == 9)
+			if (int1 == 8 && int2 == 9)
 			{
-				break;
+				continue;
 			}
-			
 			putchar(',');
 			putchar(' ');
 		}
@@ -27,5 +25,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
