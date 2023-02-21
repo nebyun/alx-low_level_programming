b2fb8186375517f71c2a93b4e705e538ee6db8f0
nebyun <nebayifru@gmail.com>
2023-02-21 10:46:17 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,14 +8,14 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+');
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		return (0);
+		_putchar(0);
 	}
 	else
 	{
-		return ('-');
+		_putchar('-');
 	}
 }
