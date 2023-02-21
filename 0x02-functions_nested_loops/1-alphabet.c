b9c30824d7ddcbd578225654dfcbd27dec54a458
nebyun <nebayifru@gmail.com>
2023-02-21 09:17:21 -0800

    I sometimes suffer from insomnia.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include <main.h>
 /**
- * description: It prints letter in lowercase
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
  */
 void print_alphabet(void)
 {
