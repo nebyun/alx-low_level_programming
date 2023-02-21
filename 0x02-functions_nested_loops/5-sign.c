8ea56a4f3d29c818f8cbcd0bba8ee62ae68cac87
nebyun <nebayifru@gmail.com>
2023-02-21 10:37:23 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_sign - check c
+ * @n:checked
+ * Return: 1 if positive or 0 if not and -1 if neg
+ */
+int print_sign(int n)
+{
+	if ((n > 0)
+		return (+);
+	else if (n == 0)
+		return (0);
+	else
+		return (-);
+
+}
