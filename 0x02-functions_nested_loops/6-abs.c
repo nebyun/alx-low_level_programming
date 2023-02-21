fcb87f1a390c500a38887fc7e70ba0345dbf30ba
nebyun <nebayifru@gmail.com>
2023-02-21 11:17:32 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - check
+ * @r: intiger number
+ * Return: absolute value of r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * -1);
+}
