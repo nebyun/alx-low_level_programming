9b6b1e8242d70f595eb56929f16861865de938c3
nebyun <nebayifru@gmail.com>
2023-02-21 18:50:55 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - check description
+ * Description: adds int numbers
+ * @i : intiger i
+ * @j : intiger j
+ * Return: result
+ */
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	return (result);
+}
