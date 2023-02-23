019a1b219d3aa443dbe0429118a87ec7eeb458c9
nebyun <nebayifru@gmail.com>
2023-02-23 10:55:46 -0800

    prime factorization

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,15 +10,15 @@ int main(void)
 	p = 612852475143;
 	while (d < (p / 2))
 	{
-		if((p % 2) == 0)
+		if ((p % 2) == 0)
 		{
 			p /= 2;
 			continue;
 		}
 
-		for(d = 3 ; d < (p /2) ; d += 2)
+		for (d = 3 ; d < (p / 2) ; d += 2)
 		{
-			if ((p % d ) == 0)
+			if ((p % d) == 0)
 				p /= d;
 		}
 	}
