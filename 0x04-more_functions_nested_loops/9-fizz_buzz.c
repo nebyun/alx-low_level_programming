1b9c2a694506ee9cdb95844851831d7da0adb334
nebyun <nebayifru@gmail.com>
2023-02-23 09:09:41 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Discription : print Fizz x3, Buzz x5, FizzBuzz x53
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if(i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+}
