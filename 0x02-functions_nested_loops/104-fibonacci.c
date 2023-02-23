a73ce826b6a8c408540b28938c3f39f51e5f1862
nebyun <nebayifru@gmail.com>
2023-02-23 05:47:22 -0800

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print fibonacci numbers up to 98 starting with 1 and 2
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long f1, f2, sum;
+
+	f1 = 1;
+	f2 = 2;
+
+	for (i = 0 ; i < 98 ; i++)
+	{
+		if (i == 0)
+		{
+			printf("%ld", f1);
+			printf(", ");
+		}
+		else if (i == 1)
+		{
+			printf("%ld", f2);
+			printf(", ");
+		}
+		else
+		{
+			sum = f1 + f2;
+			printf("%lu", sum);
+			f1 = f2;
+			f2 = sum;
+			if (i == 97)
+				printf("\n");
+			else
+				printf(", ");
+		}
+	}
+	return (0);
+}
