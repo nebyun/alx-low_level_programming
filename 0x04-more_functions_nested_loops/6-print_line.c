76e17a85c2a21399e7634d3c13acd160acb275d0
nebyun <nebayifru@gmail.com>
2023-02-23 08:20:18 -0800

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - check 
+ * @n : print _ n number of times
+ * Return: no return value
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
