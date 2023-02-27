1105923f8fc12c2ce74040a2d0fb6cc66f952250
nebyun <nebayifru@gmail.com>
2023-02-27 10:40:02 -0800

    print the second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - display the second half of string
+ * @str : String to be displayed
+ * Return: Nouting
+ */
+void puts_half(char *str)
+{
+	int i, len, n;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+	for (i = n; i < len ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
