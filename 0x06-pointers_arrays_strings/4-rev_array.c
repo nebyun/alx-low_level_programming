4377ad39c3019b8a30e8ba6768acb767c5317a61
nebyun <nebayifru@gmail.com>
2023-03-02 05:44:05 -0800

    Reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - function used to reverse array
+ * @a: array to be reversed
+ * @n: numbers to be reversed
+ * Return: Nouting
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+	
+	for (index = n - 1; index > n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
