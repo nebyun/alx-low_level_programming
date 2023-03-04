2f5b511f09bfcc1fb698921bea7b454c3c88d5f9
nebyun <nebayifru@gmail.com>
2023-03-04 10:42:34 -0800

    Buffer add

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,43 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_buffer - prints the bytes from a buffer
- * @b: byte numbers
- * @size: byte size
- * Return: Nouting
+ * print_buffer - Print a buffer 10 bytes per line.
+ * @b: Buffer address.
+ * @size: Number of characters to be printed.
  */
 void print_buffer(char *b, int size)
 {
-	int i = 0, j;
+	int j, k, l;
 
-	if (size < 0)
-	{
+	if (size <= 0)
 		printf("\n");
-		return;
-	}
-	while (i < size)
+	else
 	{
-		if (i % 10 == 0)
-			printf("%08x: ", i);
-		for (j = i; j < i + 9; j += 2)
+		for (j = 0; j < size; j += 10)
 		{
-			if ((j < size) && ((j + 1) < size))
-				printf("%02x%02x: ", b[j], b[j + 1]);
-			else
-			{
-				while (++j <= i + 10)
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+				if (k % 2 == 0)
 					printf(" ");
-				printf(" ");
-			}
-		}
-		for (j = i; j < i + 9 && j < size; j++)
-		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
+			if (k < size)
+				printf("%.2x", *(b + k));
 			else
-				printf(".");
+				printf("  ");
+		}
+		printf(" ");
+		for (l = j; l < j + 10; l++)
+		{
+			if (l >= size)
+				break;
+			if (*(b + l) < 32 || *(b + l) > 126)
+				printf("%c", '.');
+			else
+				printf("%c", *(b + l));
 		}
 		printf("\n");
-		i += 10;
 	}
 }
