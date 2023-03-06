7f5a47ebdcc95e29cd62cf2730bc2eb5059b616f
nebyun <nebayifru@gmail.com>
2023-03-06 05:39:54 -0800

    copy memory address

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy- function that copies memory areas constant bytes.
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: integer variable
+ * Return: copied two dimentional array.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest = src;
+		i++;
+	}
+	return (dest);
+}
