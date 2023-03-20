0d17fc69aad5fc1719d3f9a2dc0cc250b4305e20
nebyun <nebayifru@gmail.com>
2023-03-20 12:20:07 -0700

    0. size abbrivated to 1024, 1. macro PI to 3.14159265359, 2. print file name in litrals, 3. Absolute value Macro, 4. SUM macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Predefined macros for printing
+ * file name in string litral
+ * Return: Nouting
+ */
+int main()
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
