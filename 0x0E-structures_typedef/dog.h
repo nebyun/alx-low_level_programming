17f3e2b2c41ea9f70e112fe0cee43c8df3b1f68a
nebyun <nebayifru@gmail.com>
2023-03-21 07:36:48 -0700

    new Typedef for dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,13 +17,14 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 /**
  * dog_t - Typedef for structure dog
  */
 
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 
 #endif
