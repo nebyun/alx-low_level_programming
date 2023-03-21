811f7833fd82b3441f3056b4b284d1b056c8c4bb
nebyun <nebayifru@gmail.com>
2023-03-21 02:48:42 -0700

    structure for dog with name, age and owner

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct dog - dog properties
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Discription: a dog structure with name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
