52d634db5e2e325b70c9434f9affcf73c6e01db9
nebyun <nebayifru@gmail.com>
2023-03-22 18:59:34 -0700

    program that performs simple mathimatical operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+* op_add - compute sum of two numbers
+* @a:number one
+* @b:number two
+*
+* Return:sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - substraction of two numbers
+* @a: number one
+* @b: number two
+*
+* Return: Difference of a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - Returns the multiplication of two numbers
+* @a: number one
+* @b: number two
+*
+* Return: multiplication of a and b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - division of two numbers.
+* @a: number one
+* @b: number two
+*
+* Return: devision of a and b.
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - remainder of numbers.
+* @a: number one
+* @b: number two
+*
+* Return: remainder when deviding a by b.
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
