05f780bb25c08ffabf24823c6ba875d248760a8f
Nebyun <nebayifru@gmail.com>
2023-04-09 07:37:56 -0700

    a function that converts a binary number to an unsigned int, a function that prints the binary representation of a number, a function that returns the value of a bit at a given index...

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * get_bit - Gets the value of a bit.
+ * @n: bit.
+ * @index: The index to get the value start at 0.
+ * Return: If an error occurs - -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
+}
