060738654c8dc79d1c72934d731d9c5e1c8175cb
Nebyun <nebayifru@gmail.com>
2023-04-10 19:40:44 -0700

    a function that reads a text file and prints it to the POSIX standard output,  a function that creates a file,  a function that appends text at the end of a file,  a function that appends text at the end of a file, a program that displays the information contained in the ELF

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text - end of a file.
+ * @filename: pointer to the name of file
+ * @text_content: The string to add at end.
+ * Return: If the function fails or filename is NULL - -1.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
